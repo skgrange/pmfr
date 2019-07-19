b76e7f323df3c626e5d954667032d845ebd130da
skgrange <s.k.grange@gmail.com>
2019-07-19 14:56:13 +0200

    :sparkles: read_pmf_diagnostics now handles multiple runs & much variable name cleaning

--- R/read_pmf_diagnostics.R
+++ R/read_pmf_diagnostics.R
@@ -23,8 +23,8 @@ read_pmf_diagnostics <- function(file, tz = "UTC") {
     scaled_residual_analysis = format_scaled_residual_analysis(text),
     scaled_residual_analysis_sum_d = format_scaled_residual_analysis_sum_d(text),
     ks_test = format_ks_test(text),
-    scaled_residuals_by_species = format_scaled_residuals_by_species(text, tz = tz),
-    scaled_residuals_by_date = format_scaled_residuals_by_date(text, tz = tz)
+    scaled_outlier_residuals_by_species = format_scaled_residuals_by_species(text, tz = tz),
+    scaled_outlier_residuals_by_date = format_scaled_residuals_by_date(text, tz = tz)
   )
   
   return(list_components)
@@ -51,12 +51,15 @@ format_analysis_summary <- function(text) {
     stringr::str_trim() %>% 
     stringr::str_remove("^,")
   
+  # Make a nicely formated tibble
   df <- matrix_text[, 2] %>% 
     purrr::set_names(matrix_text[, 1]) %>% 
     tibble::enframe() %>% 
     tidyr::spread(name, value) %>% 
     dplyr::mutate_all(~if_else(. == "", NA_character_, .)) %>% 
-    mutate(time_of_run = lubridate::mdy_hm(time_of_run, tz = "UTC"))
+    mutate(time_of_run = lubridate::mdy_hm(time_of_run, tz = "UTC"),
+           concentration_file = stringr::str_replace_all(concentration_file, "\\\\", "/"),
+           uncertainty_file = stringr::str_replace_all(uncertainty_file, "\\\\", "/"))
   
   return(df)
   
@@ -70,7 +73,19 @@ format_input_data_statistics <- function(text) {
   
   df <- text[index_start:index_end] %>% 
     .[. != ""] %>% 
-    readr::read_csv()
+    readr::read_csv() %>% 
+    purrr::set_names(
+      c(
+        "species", "category", "signal_to_noise", "minimum", "lower_quartile",
+        "median", "upper_quartile", "maximum", "percent_modelled_samples", 
+        "percent_raw_samples"
+      )
+    ) %>% 
+    mutate(percent_modelled_samples = stringr::str_remove(percent_modelled_samples, "%"),
+           percent_raw_samples = stringr::str_remove(percent_raw_samples, "%"),
+           percent_modelled_samples = as.numeric(percent_modelled_samples),
+           percent_raw_samples = as.numeric(percent_raw_samples),
+           category = stringr::str_to_lower(category))
   
   return(df)
   
@@ -118,7 +133,8 @@ format_base_run_summary_table <- function(text) {
       c("model_run", "q_robust", "q_true", "converged", "number_of_steps", 
         "q_true_q_exp")
     ) %>% 
-    mutate(converged = if_else(converged == "Yes", TRUE, FALSE))
+    mutate(model_run = as.integer(model_run),
+           converged = if_else(converged == "Yes", TRUE, FALSE))
   
   return(df)
   
@@ -127,14 +143,19 @@ format_base_run_summary_table <- function(text) {
 
 format_ks_test <- function(text) {
   
-  # To-do, vectorise this
-  
   index_start <- stringr::str_which(text, "Regression diagnostics") + 1
-  index_end <- stringr::str_which(text, "Scaled residuals beyond")[1] - 1
+  index_end <- stringr::str_which(text, "dates by species") - 1
   
-  df <- text[index_start:index_end] %>% 
-    .[. != ""] %>% 
-    readr::read_csv(skip = 1)
+  # Filter to the table pieces, then read as a data frame
+  df <- purrr::map2(index_start, index_end, ~text[.x:.y]) %>% 
+    purrr::map_dfr(readr::read_csv, skip = 1, .id = "model_run") %>% 
+    mutate(model_run = as.integer(model_run)) %>% 
+    purrr::set_names(
+      c(
+        "model_run", "species", "intercept", "slope", "standard_error", 
+        "r_squared", "statistic", "p_value"
+      )
+    )
   
   return(df)
   
@@ -182,17 +203,16 @@ format_scaled_residual_analysis_sum_d <- function(text) {
 
 
 format_scaled_residuals_by_species <- function(text, tz) {
-  
-  # To-do, vectorise this
  
   index_start <- stringr::str_which(text, "dates by species") + 1
   index_end <- stringr::str_which(text, "species by date") - 1
   
-  df <- text[index_start:index_end] %>% 
-    .[. != ""] %>% 
-    readr::read_csv() %>% 
-    purrr::set_names(c("species", "date", "residual")) %>% 
-    mutate(date = lubridate::mdy_hm(date, tz = tz))
+  # Filter to the table pieces, then read as a data frame
+  df <- purrr::map2(index_start, index_end, ~text[.x:.y]) %>% 
+    purrr::map_dfr(readr::read_csv, .id = "model_run") %>% 
+    purrr::set_names(c("model_run", "species", "date", "residual")) %>% 
+    mutate(model_run = as.integer(model_run),
+           date = lubridate::mdy_hm(date, tz = tz))
   
   return(df)
   
@@ -201,15 +221,19 @@ format_scaled_residuals_by_species <- function(text, tz) {
 
 format_scaled_residuals_by_date <- function(text, tz) {
   
-  # To-do, vectorise this
-  
+  # Get indices
   index_start <- stringr::str_which(text, "species by date") + 1
+  index_end <- stringr::str_which(text, "\\*\\*\\*\\* Analysis of") - 1
   
-  df <- text[index_start:length(text)] %>% 
-    .[. != ""] %>% 
-    readr::read_csv() %>% 
-    purrr::set_names(c("date", "species", "residual")) %>% 
-    mutate(date = lubridate::mdy_hm(date, tz = tz))
+  # Drop first element which is the header for the section and the final element
+  # is the end of the last table
+  index_end <- c(index_end[-1], length(text))
+  
+  df <- purrr::map2(index_start, index_end, ~text[.x:.y]) %>% 
+    purrr::map_dfr(readr::read_csv, .id = "model_run") %>% 
+    purrr::set_names(c("model_run", "date", "species", "residual")) %>% 
+    mutate(model_run = as.integer(model_run),
+           date = lubridate::mdy_hm(date, tz = tz))
   
   return(df)
   
