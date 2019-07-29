4a778db6144a613cad0f1871b1ba373252ae646d
skgrange <s.k.grange@gmail.com>
2019-07-29 14:10:29 +0200

    :sparkles: enhance exporter by adding some tests before exporting

--- R/export_pmf_input_data.R
+++ R/export_pmf_input_data.R
@@ -4,19 +4,43 @@
 #' 
 #' @param file File name to export data to. 
 #' 
+#' @param zero_check Should the table be checked for values less than and equal 
+#' to 0? 
+#' 
 #' @author Stuart K. Grange
 #' 
 #' @return Invisible tibble. 
 #' 
 #' @export
-export_pmf_input_data <- function(df, file) {
+export_pmf_input_data <- function(df, file, zero_check = TRUE) {
   
   # Checks
-  # Missing-ness
-  # Dates
-  # Negatives
+  # No type check for date
+  if (names(df)[1] != "date") {
+    stop("First variable of the input must be called `date`...", call. = FALSE)
+  }
   
+  if (anyNA(df)) {
+    stop("Missing values (`NA`) are not allowed...", call. = FALSE)
+  }
+  
+  types <- df[-1] %>% 
+    purrr::map_chr(class) %>% 
+    unique()
+  
+  if (!all(types %in% c("numeric", "integer"))) {
+    stop("All columns apart from `date` must be numeric...", call. = FALSE)
+  }
+  
+  if (zero_check) {
+    if (any(df[, -1] <= 0)) {
+      stop("Values which are 0 or negative are not allowed...", call. = FALSE)
+    } 
+  }
+  
+  # Export file
   readr::write_delim(df, file, delim = "\t")
+  
   return(invisible(df))
   
 }
