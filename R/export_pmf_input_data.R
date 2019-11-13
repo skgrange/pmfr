6c280e57b82c9e463c54066f0b55cba2f2bcb51a
skgrange <s.k.grange@gmail.com>
2019-11-13 08:38:43 +0100

    :sparkles: enhance table exporter to include date formatting

--- R/export_pmf_input_data.R
+++ R/export_pmf_input_data.R
@@ -4,6 +4,8 @@
 #' 
 #' @param file File name to export data to. 
 #' 
+#' @param format_date Should the dates be formatted? 
+#' 
 #' @param zero_check Should the table be checked for values less than and equal 
 #' to 0? 
 #' 
@@ -12,10 +14,10 @@
 #' @return Invisible tibble. 
 #' 
 #' @export
-export_pmf_input_data <- function(df, file, zero_check = TRUE) {
+export_pmf_input_data <- function(df, file, format_date = TRUE, zero_check = TRUE) {
   
   # Checks
-  # No type check for date
+  # No type check for date yet
   if (names(df)[1] != "date") {
     stop("First variable of the input must be called `date`...", call. = FALSE)
   }
@@ -38,7 +40,14 @@ export_pmf_input_data <- function(df, file, zero_check = TRUE) {
     } 
   }
   
-  # Export file
+  # Format date
+  if (all(lubridate::hour(df$date) == 0)) {
+    df$date <- format(df$date, format = "%Y-%m-%d")
+  } else {
+    df$date <- format(df$date, format = "%Y-%m-%d %H:%M:%S")
+  }
+  
+  # Export tab delimeted file
   readr::write_delim(df, file, delim = "\t")
   
   return(invisible(df))
