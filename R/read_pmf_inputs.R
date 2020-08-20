3b998aec8ba1ddcf1416833c12d92561082db545
skgrange <s.k.grange@gmail.com>
2020-08-20 18:07:10 +0200

    :sparkles: read_pmf_inputs does not require error file now

--- R/read_pmf_inputs.R
+++ R/read_pmf_inputs.R
@@ -2,7 +2,7 @@
 #' 
 #' @param file_concentrations File name of concentration file. 
 #' 
-#' @param file_errors File name of error file. 
+#' @param file_errors File name of error file. This is optional. 
 #' 
 #' @param tz Time zone of dates. 
 #' 
@@ -14,19 +14,23 @@
 read_pmf_inputs <- function(file_concentrations, file_errors, tz = "UTC") {
   
   # Load concentrations
-  df_concentrations <- file_concentrations %>%
+  df <- file_concentrations %>%
     readr::read_delim(delim = "\t", col_types = readr::cols()) %>%
     mutate(date = lubridate::ymd(date, tz = tz)) %>% 
     tidyr::pivot_longer(-date, names_to = "variable")
   
   # Load errors
-  df_errors <- file_errors %>% 
-    readr::read_delim(delim = "\t", col_types = readr::cols()) %>% 
-    mutate(date = lubridate::ymd(date, tz = tz)) %>% 
-    tidyr::pivot_longer(-date, names_to = "variable", values_to = "error")
-  
-  # Join
-  df <- left_join(df_concentrations, df_errors, by = c("date", "variable"))
+  if (!missing("file_errors")) {
+    
+    df_errors <- file_errors %>% 
+      readr::read_delim(delim = "\t", col_types = readr::cols()) %>% 
+      mutate(date = lubridate::ymd(date, tz = tz)) %>% 
+      tidyr::pivot_longer(-date, names_to = "variable", values_to = "error")
+    
+    # Join
+    df <- left_join(df, df_errors, by = c("date", "variable"))
+    
+  }
   
   return(df)
   
