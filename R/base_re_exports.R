e5a5581b9822a9f18232ffbbf4622e29dba070dc
skgrange <s.k.grange@gmail.com>
2022-06-22 15:54:06 +0200

    :book: add names to pseudo functions

--- R/base_re_exports.R
+++ R/base_re_exports.R
@@ -1,4 +1,6 @@
 #' Pseudo-function to re-export functions from the \strong{utils} package. 
 #' 
+#' @name utils functions
+#' 
 #' @importFrom utils type.convert tail
 NULL
--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,6 @@
 #' Squash the global variable notes when building a package.
+#' 
+#' @name zzz
 #'
 if (getRversion() >= "2.15.1") {
 
